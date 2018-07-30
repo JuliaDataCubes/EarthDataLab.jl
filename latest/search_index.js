c6e455f25c1b41d8188953af2ca3e00c6bb7f34f
autodocs <autodocs>
2018-07-30 14:28:11 +0000

    build based on b737ac3

--- latest/search_index.js
+++ latest/search_index.js
@@ -457,19 +457,27 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
+    "location": "plotting.html#ESDLPlots.plotMAP",
+    "page": "Plotting",
+    "title": "ESDLPlots.plotMAP",
+    "category": "function",
+    "text": "plotMAP(cube::AbstractCubeData; dmin=datamin, dmax=datamax, colorm=colormap(\"oranges\"), oceancol=colorant\"darkblue\", misscol=colorant\"gray\", kwargs...)\n\nMap plotting tool for cube objects, can be called on any type of cube data\n\nKeyword arguments\n\ndmin, dmax Minimum and maximum value to be used for color transformation\ncolorm colormap to be used. Find a list of colormaps in the Colors.jl package\noceancol color to fill the ocean with, defaults to colorant\"darkblue\"\nmisscol color to represent missing values, defaults to colorant\"gray\"\nsymmetric make the color scale symmetric around zero\nxaxis which axis should be used for x axis, defaults to LonAxis\nyaxis which axis should be used for y axis, defaults to LatAxis\ndim=value can set other dimensions to certain values, for example var=\"air_temperature_2m\" will fix the variable for the resulting plot\n\nIf a dimension is neither longitude or latitude and is not fixed through an additional keyword, a slider or dropdown menu will appear to select the axis value.\n\nIf the properties field of cube contains a \"labels\" field with a dictionary mapping field values to the name of the class represented.\n\n\n\n"
+},
+
+{
     "location": "plotting.html#Plot-geographical-maps-1",
     "page": "Plotting",
     "title": "Plot geographical maps",
     "category": "section",
-    "text": "Map plotting is generally done using the plotMAP function:plotMAP(cube::AbstractCubeData; dmin=datamin, dmax=datamax, colorm=colormap(\"oranges\"), oceancol=colorant\"darkblue\", misscol=colorant\"gray\", kwargs...)Map plotting tool for cube objects, can be called on any type of cube data"
+    "text": "Map plotting is generally done using the plotMAP function:plotMAPIf a dimension is neither longitude or latitude and is not fixed through an additional keyword, a slider or dropdown menu will appear to select the axis value.Here is an example on how to plot a map. The keyword arguments specify the time step (time=1) and the variable (var=1).\ncdata=getCubeData(ds,variable=[\"air_temperature_2m\",\"gross_primary_productivity\"])\nplotMAP(cdata,time=1,var=1)Inside a Jupyter notebook, the keyword arguments can be omitted and sliders or dropdown menus will be shown to select the desired values."
 },
 
 {
-    "location": "plotting.html#Keyword-arguments-1",
+    "location": "plotting.html#ESDLPlots.plotMAPRGB",
     "page": "Plotting",
-    "title": "Keyword arguments",
-    "category": "section",
-    "text": "dmin, dmax Minimum and maximum value to be used for color transformation\ncolorm colormap to be used. Find a list of colormaps in the Colors.jl package\noceancol color to fill the ocean with, defaults to colorant\"darkblue\"\nmisscol color to represent missing values, defaults to colorant\"gray\"\nsymmetric make the color scale symmetric around zero\nlabels given a list of labels this will create a plot with a non-continouous color scale where integer cube values [1..N] are mapped to the given labels.\ndim=value can set other dimensions to certain values, for example var=\"air_temperature_2m\" will fix the variable for the resulting plotIf a dimension is neither longitude or latitude and is not fixed through an additional keyword, a slider or dropdown menu will appear to select the axis value.Here is an example on how to plot a map. The keyword arguments specify the time step (time=1) and the variable (var=1).\ncdata=getCubeData(ds,variable=[\"air_temperature_2m\",\"gross_primary_productivity\"])\nplotMAP(cdata,time=1,var=1)Inside a Jupyter notebook, the keyword arguments can be omitted and sliders or dropdown menus will be shown to select the desired values."
+    "title": "ESDLPlots.plotMAPRGB",
+    "category": "function",
+    "text": "plotMAPRGB(cube::AbstractCubeData; dmin=datamin, dmax=datamax, colorm=colormap(\"oranges\"), oceancol=colorant\"darkblue\", misscol=colorant\"gray\", kwargs...)\n\nMap plotting tool for colored plots that use up to 3 variables as input into the several color channels. Several color representations from the Colortypes.jl package are supported, so that besides RGB (XYZ)-plots one can create HSL, HSI, HSV or Lab and Luv plots.\n\nKeyword arguments\n\ndmin, dmax Minimum and maximum value to be used for color transformation, can be either a single value or a tuple, when min/max values are given for each channel\nrgbAxis which axis should be used to select RGB channels from\noceancol color to fill the ocean with, defaults to colorant\"darkblue\"\nmisscol color to represent missing values, defaults to colorant\"gray\"\nlabels given a list of labels this will create a plot with a non-continouous color scale where integer cube values [1..N] are mapped to the given labels.\ncType ColorType to use for the color representation. Can be one of RGB, XYZ, Lab, Luv, HSV, HSI, HSL\ndim=value can set other dimensions to certain values, for example var=\"air_temperature_2m\" will fix the variable for the resulting plot\n\nIf a dimension is neither longitude or latitude and is not fixed through an additional keyword, a slider or dropdown menu will appear to select the axis value.\n\n\n\n"
 },
 
 {
@@ -477,15 +485,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Plotting",
     "title": "RGB Maps",
     "category": "section",
-    "text": "A common method to plot several variables at once in a single map is an RGB map. This is possible through the plotMAPRGB function.plotMAPRGB(cube::AbstractCubeData; dmin=datamin, dmax=datamax, colorm=colormap(\"oranges\"), oceancol=colorant\"darkblue\", misscol=colorant\"gray\", kwargs...)Map plotting tool for colored plots that use up to 3 variables as input into the several color channels. Several color representations from the Colortypes.jl package are supported, so that besides RGB (XYZ)-plots one can create HSL, HSI, HSV or Lab and Luv plots."
-},
-
-{
-    "location": "plotting.html#Keyword-arguments-2",
-    "page": "Plotting",
-    "title": "Keyword arguments",
-    "category": "section",
-    "text": "dmin, dmax Minimum and maximum value to be used for color transformation, can be either a single value or a tuple, when min/max values are given for each channel\nrgbAxis which axis should be used to select RGB channels from\noceancol color to fill the ocean with, defaults to colorant\"darkblue\"\nmisscol color to represent missing values, defaults to colorant\"gray\"\nlabels given a list of labels this will create a plot with a non-continouous color scale where integer cube values [1..N] are mapped to the given labels.\ncType ColorType to use for the color representation. Can be one of RGB, XYZ, Lab, Luv, HSV, HSI, HSL\ndim=value can set other dimensions to certain values, for example var=\"air_temperature_2m\" will fix the variable for the resulting plotIf a dimension is neither longitude or latitude and is not fixed through an additional keyword, a slider or dropdown menu will appear to select the axis value.For example, if we want to plot GPP, NEE and TER as an RGB map for South America, we can do the following:d=getCubeData(ds,variable=\"Biosphere\",region=\"South America\")\nusing ColorTypes\nplotMAPRGB(d,c1=\"gross_primary_productivity\",\n             c2=\"net_ecosystem_exchange\",\n             c3=\"terrestrial_ecosystem_respiration\",\n             cType=Lab,\n             time=100)"
+    "text": "A common method to plot several variables at once in a single map is an RGB map. This is possible through the plotMAPRGB function.plotMAPRGBFor example, if we want to plot GPP, NEE and TER as an RGB map for South America, we can do the following:d=getCubeData(ds,variable=\"Biosphere\",region=\"South America\")\nusing ColorTypes\nplotMAPRGB(d,c1=\"gross_primary_productivity\",\n             c2=\"net_ecosystem_exchange\",\n             c3=\"terrestrial_ecosystem_respiration\",\n             cType=Lab,\n             time=100)"
 },
 
 {
@@ -497,11 +497,27 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
+    "location": "plotting.html#ESDLPlots.plotXY",
+    "page": "Plotting",
+    "title": "ESDLPlots.plotXY",
+    "category": "function",
+    "text": "plotXY(cube::AbstractCubeData; group=0, xaxis=-1, kwargs...)\n\nGeneric plotting tool for cube objects, can be called on any type of cube data.\n\nKeyword arguments\n\nxaxis which axis is to be used as x axis. Can be either an axis Datatype or a string. Short versions of axes names are possible as long as the axis can be uniquely determined.\ngroup it is possible to group the plot by a categorical axis. Can be either an axis data type or a string.\ndim=value can set other dimensions to certain values, for example lon=51.5 will fix the longitude for the resulting plot\n\nIf a dimension is not the x axis or group variable and is not fixed through an additional keyword, a slider or dropdown menu will appear to select the axis value.\n\n\n\n"
+},
+
+{
     "location": "plotting.html#XY-plots-1",
     "page": "Plotting",
     "title": "XY plots",
     "category": "section",
-    "text": "Generating x-y type plots where the x axis is one of the cube axes and the y axis is the corresponding cube value is done with the generic plotXY function.`plotXY(cube::AbstractCubeData; group=0, xaxis=-1, kwargs...)`\n\nGeneric plotting tool for cube objects, can be called on any type of cube data.\n\n### Keyword arguments\n\n* `xaxis` which axis is to be used as x axis. Can be either an axis Datatype or a string. Short versions of axes names are possible as long as the axis can be uniquely determined.\n* `group` it is possible to group the plot by a categorical axis. Can be either an axis data type or a string.\n* `dim=value` can set other dimensions to certain values, for example `lon=51.5` will fix the longitude for the resulting plot\n\nIf a dimension is not the x axis or group variable and is not fixed through an additional keyword, a slider or dropdown menu will appear to select the axis value.Here are two examples for using this function:cdata=getCubeData(ds,variable=[\"net_ecosystem_exchange\",\"gross_primary_productivity\",\"terrestrial_ecosystem_respiration\"],\nlongitude=(30.0,30.0),latitude=(50.0,52.0))\nplotXY(cdata,xaxis=\"time\",group=\"variable\",lon=31,lat=51)\nnothing # hideusing ESDL # hide\nusing ESDLPlots\ngr()\nimport Documenter # hide\nds=Cube() # hide\ncdata=getCubeData(ds,variable=[\"net_ecosystem_exchange\",\"gross_primary_productivity\",\"terrestrial_ecosystem_respiration\"],\nlongitude=(30.0,30.0),latitude=(50.0,52.0))\np=plotXY(cdata,xaxis=\"time\",group=\"variable\",lon=31,lat=51)\nb=IOBuffer()\nshow(b,MIME\"text/html\"(),p)\nDocumenter.Documents.RawHTML(String(take!(b)))This is a time series plot, grouped by variables for a specific longitude/latitude.m=reduceCube(mean,cdata,TimeAxis)\nplotXY(m,xaxis=\"variable\",group=\"lat\",lon=30)using ESDL # hide\nusing ESDLPlots\ngr()\nimport Documenter # hide\nds=Cube() # hide\ncdata=getCubeData(ds,variable=[\"net_ecosystem_exchange\",\"gross_primary_productivity\",\"terrestrial_ecosystem_respiration\"],\nlongitude=(30.0,30.0),latitude=(50.0,52.0))\nm=reduceCube(mean,cdata,TimeAxis, max_cache=1e8)\np=plotXY(m,xaxis=\"variable\",group=\"lat\",lon=30)\nb=IOBuffer()\nshow(b,MIME\"text/html\"(),p)\nDocumenter.Documents.RawHTML(String(take!(b)))"
+    "text": "Generating x-y type plots where the x axis is one of the cube axes and the y axis is the corresponding cube value is done with the generic plotXY function.plotXYHere are two examples for using this function:cdata=getCubeData(ds,variable=[\"net_ecosystem_exchange\",\"gross_primary_productivity\",\"terrestrial_ecosystem_respiration\"],\nlongitude=(30.0,30.0),latitude=(50.0,52.0))\nplotXY(cdata,xaxis=\"time\",group=\"variable\",lon=31,lat=51)\nnothing # hideusing ESDL # hide\nusing ESDLPlots\ngr()\nimport Documenter # hide\nds=Cube() # hide\ncdata=getCubeData(ds,variable=[\"net_ecosystem_exchange\",\"gross_primary_productivity\",\"terrestrial_ecosystem_respiration\"],\nlongitude=(30.0,30.0),latitude=(50.0,52.0))\np=plotXY(cdata,xaxis=\"time\",group=\"variable\",lon=31,lat=51)\nb=IOBuffer()\nshow(b,MIME\"text/html\"(),p)\nDocumenter.Documents.RawHTML(String(take!(b)))This is a time series plot, grouped by variables for a specific longitude/latitude.m=reduceCube(mean,cdata,TimeAxis)\nplotXY(m,xaxis=\"variable\",group=\"lat\",lon=30)using ESDL # hide\nusing ESDLPlots\ngr()\nimport Documenter # hide\nds=Cube() # hide\ncdata=getCubeData(ds,variable=[\"net_ecosystem_exchange\",\"gross_primary_productivity\",\"terrestrial_ecosystem_respiration\"],\nlongitude=(30.0,30.0),latitude=(50.0,52.0))\nm=reduceCube(mean,cdata,TimeAxis, max_cache=1e8)\np=plotXY(m,xaxis=\"variable\",group=\"lat\",lon=30)\nb=IOBuffer()\nshow(b,MIME\"text/html\"(),p)\nDocumenter.Documents.RawHTML(String(take!(b)))"
+},
+
+{
+    "location": "plotting.html#ESDLPlots.plotScatter",
+    "page": "Plotting",
+    "title": "ESDLPlots.plotScatter",
+    "category": "function",
+    "text": "plotScatter(cube::AbstractCubeData; vsaxis=VariableAxis, alongaxis=0, group=0, xaxis=0, yaxis=0, kwargs...)\n\nGeneric plotting tool for cube objects to generate scatter plots, like variable A against variable B. Can be called on any type of cube data.\n\nKeyword arguments\n\nvsaxis determines the axis from which the x and y variables are drawn.\nalongaxis determines the axis along which the variables are plotted. E.g. if you choose TimeAxis, a dot will be plotted for each time step.\nxaxis index or value of the variable to plot on the x axis\nyaxis index or value of the variable to plot on the y axis\ngroup it is possible to group the plot by an axis. Can be either an axis data type or a string. Caution: This will increase the number of plotted data points\ndim=value can set other dimensions to certain values, for example lon=51.5 will fix the longitude for the resulting plot\n\nIf a dimension is not the vsaxis or alongaxis or group and is not fixed through an additional keyword, a slider or dropdown menu will appear to select the axis value.\n\n\n\n"
 },
 
 {
@@ -509,15 +525,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Plotting",
     "title": "Scatter plots",
     "category": "section",
-    "text": "In order to do scatter plots, i.e. plotting variable A against variable B one can use the plotScatter function.plotScatter(cube::AbstractCubeData; vsaxis=VariableAxis, alongaxis=0, group=0, xaxis=0, yaxis=0, kwargs...)Generic plotting tool for cube objects to generate scatter plots, like variable A against variable B. Can be called on any type of cube data."
-},
-
-{
-    "location": "plotting.html#Keyword-arguments-3",
-    "page": "Plotting",
-    "title": "Keyword arguments",
-    "category": "section",
-    "text": "vsaxis determines the axis from which the x and y variables are drawn.\nalongaxis determines the axis along which the variables are plotted. E.g. if you choose TimeAxis, a dot will be plotted for each time step.\nxaxis index or value of the variable to plot on the x axis\nyaxis index or value of the variable to plot on the y axis\ngroup it is possible to group the plot by an axis. Can be either an axis data type or a string. Caution: This will increase the number of plotted data points\ndim=value can set other dimensions to certain values, for example lon=51.5 will fix the longitude for the resulting plotIf a dimension is not the vsaxis or alongaxis or group and is not fixed through an additional keyword, a slider or dropdown menu will appear to select the axis value.A short example is shown here:cdata=getCubeData(ds,variable=[\"net_ecosystem_exchange\",\"gross_primary_productivity\",\"terrestrial_ecosystem_respiration\"],\nlongitude=(30.0,30.0),latitude=(50.0,52.0))\np=plotScatter(cdata,alongaxis=TimeAxis,xaxis=1,yaxis=2,group=\"lat\",lon=30)using ESDL # hide\nusing ESDLPlots\ngr()\nimport Documenter # hide\nds=Cube() # hide\ncdata=getCubeData(ds,variable=[\"net_ecosystem_exchange\",\"gross_primary_productivity\",\"terrestrial_ecosystem_respiration\"],\nlongitude=(30.0,30.0),latitude=(50.0,52.0))\np=plotScatter(cdata,alongaxis=TimeAxis,xaxis=\"net_ecosystem_exchange\",yaxis=\"gross_primary_productivity\",group=\"lat\",lon=30.0)\nb=IOBuffer()\nshow(b,MIME\"text/html\"(),p)\nDocumenter.Documents.RawHTML(String(take!(b)))"
+    "text": "In order to do scatter plots, i.e. plotting variable A against variable B one can use the plotScatter function.plotScatterA short example is shown here:cdata=getCubeData(ds,variable=[\"net_ecosystem_exchange\",\"gross_primary_productivity\",\"terrestrial_ecosystem_respiration\"],\nlongitude=(30.0,30.0),latitude=(50.0,52.0))\np=plotScatter(cdata,alongaxis=TimeAxis,xaxis=1,yaxis=2,group=\"lat\",lon=30)using ESDL # hide\nusing ESDLPlots\ngr()\nimport Documenter # hide\nds=Cube() # hide\ncdata=getCubeData(ds,variable=[\"net_ecosystem_exchange\",\"gross_primary_productivity\",\"terrestrial_ecosystem_respiration\"],\nlongitude=(30.0,30.0),latitude=(50.0,52.0))\np=plotScatter(cdata,alongaxis=TimeAxis,xaxis=\"net_ecosystem_exchange\",yaxis=\"gross_primary_productivity\",group=\"lat\",lon=30.0)\nb=IOBuffer()\nshow(b,MIME\"text/html\"(),p)\nDocumenter.Documents.RawHTML(String(take!(b)))"
 },
 
 {
@@ -637,7 +645,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Loading and saving results",
     "title": "ESDL.Cubes.saveCube",
     "category": "function",
-    "text": "saveCube(c::AbstractCubeData, name::String)\n\nPermanently saves a data cube to disk by either moving the folder out of the tmp directory (for TempCubes) or by storing the data to disk (for CubeMems)\n\n\n\nsaveCube(c::AbstractCubeData, name::String)\n\nPermanently saves a data cube to disk by either moving the folder out of the tmp directory (for TempCubes) or by storing the data to disk (for CubeMems)\n\n\n\nsaveCube(cube,name::String)\n\nSave a TempCube or CubeMem to the folder name in the ESDL working directory.\n\nSee also loadCube, ESDLdir\n\n\n\n"
+    "text": "saveCube(c::AbstractCubeData, name::String)\n\nPermanently saves a data cube to disk by either moving the folder out of the tmp directory (for TempCubes) or by storing the data to disk (for CubeMems)\n\n\n\nsaveCube(cube,name::String)\n\nSave a TempCube or CubeMem to the folder name in the ESDL working directory.\n\nSee also loadCube, ESDLdir\n\n\n\nsaveCube(c::AbstractCubeData, name::String)\n\nPermanently saves a data cube to disk by either moving the folder out of the tmp directory (for TempCubes) or by storing the data to disk (for CubeMems)\n\n\n\n"
 },
 
 {
