cd5824fda21d361db745635a385e20c6ff69283d
Felix Cremer <felix.cremer@uni-jena.de>
2021-01-13 14:42:10 +0100

    Add tests for wrap=nothing

---
+++ test/shapes.jl
@@ -0,0 +1,9 @@
+#Test whether the cubefromshape for a shapefile with a different CRS works when we set wrap to nothing
+@testset "Load shapefile with wrap=nothing" begin
+cube = Cube("data/s1_jurua_singlelake_large.nc")
+shape = cubefromshape("data/s1_jurua_singlelake_select.shp", cube, wrap=nothing)
+inds = findall(isequal.(cube.data), 1)
+#This test makes sure, that the indices are inside the lake values
+#We don't compare the values directly, because the rasterization can still be shifted by half a pixel
+@test [cube[ind.I...] for ind in inds] .<0.015
+end
